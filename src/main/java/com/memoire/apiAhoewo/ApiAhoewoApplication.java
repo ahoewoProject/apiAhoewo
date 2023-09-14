2803078240620c09cc0d3c61711dc6cf7f95853e
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-09-14 02:02:48 +0000

    Lancement projet { Ahoewo }

---
+++ src/main/java/com/memoire/apiAhoewo/ApiAhoewoApplication.java
@@ -0,0 +1,13 @@
+package com.memoire.apiAhoewo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAhoewoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAhoewoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memoire/apiAhoewo/ApiAhoewoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoire.apiAhoewo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAhoewoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
